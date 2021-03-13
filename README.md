9c5e1ed9aacc0adaf8f73fa298c94ba5d4f2cd5f
marv-ink <marvin96k@gmail.com>
2021-03-13 14:50:37 +0100

    made a few cahnges

---
+++ README.md
@@ -0,0 +1,17 @@
+# Duplicate design
+
+This is a school assignment, where we were given a picture to duplicate through an actual web page
+
+## To get started
+
+In your vs terminal after cloning the repo
+
+- Type: npm install
+
+after installing all of the npm packages you are good to go. Every time you make a change in scss you have to compile to css by typing in:
+
+```bash
+gulp
+```
+
+into the vs terminal.
--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,14 @@
 let gulp = require('gulp');
 let scss = require('gulp-sass');
 let scsso = require('gulp-csso');
- 
-gulp.task('default', function() {
-return gulp.src('scss/*.scss')
- .pipe(scss())
- .pipe(gulp.dest('css'))
- .pipe(scsso())
- .pipe(gulp.dest('Minified'));
- 
+let rename = require('gulp-rename');
+
+gulp.task('default', function () {
+  return gulp
+    .src('scss/*.scss')
+    .pipe(scss())
+    .pipe(gulp.dest('css'))
+    .pipe(scsso())
+    .pipe(rename({ extname: '.min.css' }))
+    .pipe(gulp.dest('Minified'));
 });
